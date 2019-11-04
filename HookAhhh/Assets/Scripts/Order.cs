d228ee4f3fd14a2ae8994cd1da6cbd61c612a2cf
Leshka <31736716+TOPlessss@users.noreply.github.com>
2019-11-04 21:26:35 +0300

    Order

---
+++ HookAhhh/Assets/Scripts/Order.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Order : MonoBehaviour
+{
+    private int _cost = 0;
+    ArrayList order = new ArrayList();
+
+    public void addElementToOrder(Zabiv zabiv)
+    {
+        order.Add(zabiv);
+        _cost += 1200;
+    }
+
+    public void removeElementFromOrder(int id)
+    {
+        order.Remove(id);
+    }
+
+    public int getCost()
+    {
+        return _cost;
+    }
+
+    public void setCost(int cost)
+    {
+        _cost = cost;
+    }
+}
