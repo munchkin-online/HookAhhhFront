5f87837a96d3fd2b1651e0c5496bda5981ab717f
Averchenkov Nikita IKBO-02-17 <31746573+Averchenkov@users.noreply.github.com>
2019-12-05 05:31:04 +0300

    add add order to baket

---
+++ HookAhhh/Assets/Scripts/AddOrderOnBasket.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddOrderOnBasket : MonoBehaviour
+{
+    public RectTransform prefab;
+    public void OnClick()
+    {
+        var i = prefab.gameObject.transform.Find("TextId").GetComponent<Text>().text;
+        var go = GameObject.Find("Content");
+        go.GetComponent<ScrollViewAdapter>().AddOrderOnBasket(int.Parse(i));
+    }
+}
